f4ad3e0626c8e06db9b0cfa945c432f9a5392218
October Hammer <octoberhammer@gmail.com>
2018-01-02 21:40:47 +0200

    no message

--- TestTaskLivatek/PlayerViewController.swift
+++ TestTaskLivatek/PlayerViewController.swift
@@ -43,9 +43,7 @@ class PlayerViewController: UIViewController {
   
   
   
-  
 
-  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
