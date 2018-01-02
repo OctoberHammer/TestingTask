223b508cb5d94e3070c6ee6e942ed10938bb26db
October Hammer <octoberhammer@gmail.com>
2018-01-02 21:22:25 +0200

    no message

--- TestTaskLivatek/DragAndDropAndPushRecognizer.swift
+++ TestTaskLivatek/DragAndDropAndPushRecognizer.swift
@@ -31,40 +31,15 @@ class DragAndDropAndPushRecognizer {
       swipeRecognizer.direction = everyDirection
       self.swipes.append(swipeRecognizer)
       movableView.addGestureRecognizer(swipeRecognizer)
+      //swipes need to be preferable
       panGesture.require(toFail: swipeRecognizer)
     }
-    
-//    let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(DragAndDropAndPushRecognizer.pushView(_:)))
-//    swipeLeft.direction = .left
-//    movableView.addGestureRecognizer(swipeLeft)
-//
-//    let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(DragAndDropAndPushRecognizer.pushView(_:)))
-//    swipeRight.direction = .right
-//    movableView.addGestureRecognizer(swipeRight)
-//
-//    let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(DragAndDropAndPushRecognizer.pushView(_:)))
-//    swipeUp.direction = .up
-//    movableView.addGestureRecognizer(swipeUp)
-//
-//    let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(DragAndDropAndPushRecognizer.pushView(_:)))
-//    swipeDown.direction = .down
-//    movableView.addGestureRecognizer(swipeDown)
-//
-//    self.swipes = [swipeLeft, swipeRight , swipeUp, swipeDown]
-//
-//
-    
-    
-//    panGesture.require(toFail: swipeLeft)
-//    panGesture.require(toFail: swipeRight)
-//    panGesture.require(toFail: swipeUp)
-//    panGesture.require(toFail: swipeDown)
     
     movableView.addGestureRecognizer(panGesture)
   }
   
   
-  @objc func dragAndDropView(_ sender:UIPanGestureRecognizer){
+  @objc private func dragAndDropView(_ sender:UIPanGestureRecognizer){
     self.parentView.bringSubview(toFront: movableView)
     let translation = sender.translation(in: self.parentView)
     movableView.center = CGPoint(x: movableView.center.x + translation.x, y: movableView.center.y + translation.y)
@@ -72,25 +47,21 @@ class DragAndDropAndPushRecognizer {
   }
  
   
-  @objc func pushView(_ gesture: UISwipeGestureRecognizer) {
+  @objc private func pushView(_ gesture: UISwipeGestureRecognizer) {
     var animation: ()->() = {}
-    print(gesture.direction)
     switch gesture.direction {
     case UISwipeGestureRecognizerDirection.left:
-      print("Swiped left")
       animation = { self.movableView.center.x =  self.movableView.bounds.width / 2 }
     case UISwipeGestureRecognizerDirection.right:
-      print("Swiped right")
       animation = { self.movableView.center.x = self.parentView.bounds.width - self.movableView.bounds.width / 2}
     case UISwipeGestureRecognizerDirection.down:
-      print("Swiped down")
       animation = { self.movableView.center.y = self.parentView.bounds.height - self.movableView.bounds.height / 2 }
     case UISwipeGestureRecognizerDirection.up:
-      print("Swiped up")
       animation = { self.movableView.center.y = self.movableView.bounds.height / 2 }
     default:
       break
     }
+    //Animation during 0.5 seconds, with no delay, Acceleration at the Begin, deceleratin at the End
     UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut,
                    animations: animation, completion: nil)
   }
