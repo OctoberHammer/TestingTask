aedf2826f811662e5101fcbd897f34ab29df4810
October Hammer <octoberhammer@gmail.com>
2018-01-03 15:48:48 +0200

    Окончательная отладка ЖестРекогнайзера, и перемещения круглого Вью по экрану, с учетом смены ориентации девайса

--- TestTaskLivatek/ComplexGestureRecognizer/DragAndDropAndPushRecognizer.swift
+++ TestTaskLivatek/ComplexGestureRecognizer/DragAndDropAndPushRecognizer.swift
@@ -18,6 +18,9 @@ class DragAndDropAndPushRecognizer {
   weak var movableView: UIView?
   
   
+  var delegateCallBackForDragAndDrop: (CGFloat, CGFloat) -> Void = {(dx: CGFloat, dy: CGFloat) in print(dx, dy)}
+  var delegateCallBackForPush: (UISwipeGestureRecognizerDirection) -> Void = {(direction: UISwipeGestureRecognizerDirection) in print(direction)}
+  
   init(parentView: UIView, movableView: UIView) {
     self.parentView = parentView
     self.movableView = movableView
@@ -42,30 +45,32 @@ class DragAndDropAndPushRecognizer {
   @objc private func dragAndDropView(_ sender:UIPanGestureRecognizer){
     guard let parentView = self.parentView, let movableView = self.movableView else {return}
     parentView.bringSubview(toFront: movableView)
+    //parentView.sendSubview(toBack: movableView)
     let translation = sender.translation(in: parentView)
-    movableView.center = CGPoint(x: movableView.center.x + translation.x, y: movableView.center.y + translation.y)
+    self.delegateCallBackForDragAndDrop(translation.x, translation.y)
     sender.setTranslation(CGPoint.zero, in: parentView)
   }
  
   
   @objc private func pushView(_ gesture: UISwipeGestureRecognizer) {
-    var animation: ()->() = {}
-    guard let parentView = self.parentView, let movableView = self.movableView else {return}
-    switch gesture.direction {
-    case UISwipeGestureRecognizerDirection.left:
-      animation = { movableView.center.x =  movableView.bounds.width / 2 }
-    case UISwipeGestureRecognizerDirection.right:
-      animation = { movableView.center.x = parentView.bounds.width - movableView.bounds.width / 2 }
-    case UISwipeGestureRecognizerDirection.down:
-      animation = { movableView.center.y = parentView.bounds.height - movableView.bounds.height / 2 }
-    case UISwipeGestureRecognizerDirection.up:
-      animation = { movableView.center.y = movableView.bounds.height / 2 }
-    default:
-      break
-    }
-    //Animation during 0.5 seconds, with no delay, Acceleration at the Begin, deceleratin at the End
-    UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut,
-                   animations: animation, completion: nil)
+    //var animation: ()->() = {}
+    self.delegateCallBackForPush(gesture.direction)
+//    guard let parentView = self.parentView, let movableView = self.movableView else {return}
+//    switch gesture.direction {
+//    case UISwipeGestureRecognizerDirection.left:
+//      animation = { movableView.center.x =  movableView.bounds.width / 2 }
+//    case UISwipeGestureRecognizerDirection.right:
+//      animation = { movableView.center.x = parentView.bounds.width - movableView.bounds.width / 2 }
+//    case UISwipeGestureRecognizerDirection.down:
+//      animation = { movableView.center.y = parentView.bounds.height - movableView.bounds.height / 2 }
+//    case UISwipeGestureRecognizerDirection.up:
+//      animation = { movableView.center.y = movableView.bounds.height / 2 }
+//    default:
+//      break
+//    }
+//    //Animation during 0.5 seconds, with no delay, Acceleration at the Begin, deceleratin at the End
+//    UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut,
+//                   animations: animation, completion: nil)
   }
   
   
--- TestTaskLivatek/PlayerViewController.swift
+++ TestTaskLivatek/PlayerViewController.swift
@@ -11,18 +11,27 @@ import AVFoundation
 
 class PlayerViewController: UIViewController {
 
+
+  
   var player: AVAudioPlayer?
   @IBOutlet weak var circleAudioPlayer: CircleAudioPlayer!
   
   var complexGestureRecognizer: DragAndDropAndPushRecognizer?
+  var currentRelativeHorisontalPosition: CGFloat = 0.5
+  var currentRelativeVerticalPosition: CGFloat = 0.5
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.complexGestureRecognizer = DragAndDropAndPushRecognizer(parentView: self.view, movableView: self.circleAudioPlayer)
+    self.complexGestureRecognizer?.delegateCallBackForDragAndDrop = self.dragAndDropCircleAudioPlayer
+    self.complexGestureRecognizer?.delegateCallBackForPush = self.pushCircleAudioPlayerToTheEndge
+    self.circleAudioPlayer.center = CGPoint(x: self.view.bounds.midX, y: self.view.bounds.midY)
+    //1
+    NotificationCenter.default.addObserver(self, selector: #selector(deviceOrientationDidChange), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+
   }
 
 
-  
   @IBAction func useLocalFile(_ sender: UISwitch) {
     if sender.isOn {
       let audioPath = Bundle.main.path(forResource: "02 - Suzanne Vega - Tom's Diner", ofType: "mp3")
@@ -40,10 +49,52 @@ class PlayerViewController: UIViewController {
     }
     
   }
-  
-  
-  
 
+  
+  func dragAndDropCircleAudioPlayer(dx: CGFloat, dy: CGFloat) {
+    print(dx, dy)
+    if dx != 0 {
+      self.currentRelativeHorisontalPosition = self.circleAudioPlayer.center.x / self.view.bounds.maxX
+      let relativeChange = dx/self.view.bounds.maxX
+      self.circleAudioPlayer.center.x = (self.currentRelativeHorisontalPosition + relativeChange) * self.view.bounds.maxX
+    }
+    if dy != 0 {
+      self.currentRelativeVerticalPosition = self.circleAudioPlayer.center.y / self.view.bounds.maxY
+      let relativeChange = dy/self.view.bounds.maxY
+      self.circleAudioPlayer.center.y = (self.currentRelativeVerticalPosition + relativeChange) * self.view.bounds.maxY
+    }
+  }
+
+  
+  func pushCircleAudioPlayerToTheEndge(direction: UISwipeGestureRecognizerDirection) {
+    var animation: ()->() = {}
+    switch direction {
+    case UISwipeGestureRecognizerDirection.left:
+      animation = { self.circleAudioPlayer.center.x = 0 + self.circleAudioPlayer.bounds.width / 2}
+    case UISwipeGestureRecognizerDirection.right:
+      animation = { self.circleAudioPlayer.center.x = self.view.bounds.maxX -  self.circleAudioPlayer.bounds.width / 2}
+    case UISwipeGestureRecognizerDirection.down:
+      animation = { self.circleAudioPlayer.center.y = self.view.bounds.maxY - self.circleAudioPlayer.bounds.height / 2 }
+    case UISwipeGestureRecognizerDirection.up:
+      animation = { self.circleAudioPlayer.center.y = 0 + self.circleAudioPlayer.bounds.height / 2}
+    default:
+      break
+    }
+    //Animation during 0.5 seconds, with no delay, Acceleration at the Begin, deceleratin at the End
+    UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut,
+                   animations: animation, completion: nil)
+  }
+  
+  deinit {
+    NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+  }
+  
+  @objc func deviceOrientationDidChange() {
+    self.circleAudioPlayer.center.x = self.currentRelativeHorisontalPosition * self.view.bounds.maxX
+    self.circleAudioPlayer.center.y = self.currentRelativeVerticalPosition * self.view.bounds.maxY
+  }
+  
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
